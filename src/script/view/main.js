400a1f705eed6f06602fc7a7351def456f61a131
Indra Mahkota <indramahkota1@gmail.com>
2020-04-28 12:17:13 +0700

    use const variable definition

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,23 +1,27 @@
-var main = function () {
-    var searchElement = document.querySelector("#searchElement");
-    var buttonSearchElement = document.querySelector("#searchButtonElement");
-    var clubListElement = document.querySelector("#clubList");
-
-    var onButtonSearchClicked = function () {
-        var dataSource = new DataSource(renderResult, fallbackResult);
+const main = function () {
+    const searchElement = document.querySelector("#searchElement");
+    const buttonSearchElement = document.querySelector("#searchButtonElement");
+    const clubListElement = document.querySelector("#clubList");
+  
+  
+    const onButtonSearchClicked = function () {
+        const dataSource = new DataSource(renderResult, fallbackResult);
         dataSource.searchClub(searchElement.value);
     };
-
-    var renderResult = function (results) {
+  
+  
+    const renderResult = function (results) {
         clubListElement.innerHTML = "";
         results.forEach(function (club) {
-            var name = club.name;
-            var fanArt = club.fanArt;
-            var description = club.description;
-
-            var clubElement = document.createElement("div");
+            const name = club.name;
+            const fanArt = club.fanArt;
+            const description = club.description;
+  
+  
+            const clubElement = document.createElement("div");
             clubElement.setAttribute("class", "club");
-
+  
+  
             clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
                 '<div class="club-info">\n' +
                 '<h2>' + name + '</h2>\n' +
@@ -26,11 +30,13 @@ var main = function () {
             clubListElement.appendChild(clubElement);
         })
     };
-
-    var fallbackResult = function (message) {
+  
+  
+    const fallbackResult = function (message) {
         clubListElement.innerHTML = "";
         clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
     };
-
+  
+  
     buttonSearchElement.addEventListener("click", onButtonSearchClicked);
-};
+ };
