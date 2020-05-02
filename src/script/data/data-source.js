7433b992b0a04175acc88136af25dd360ef3be13
Indra Mahkota <indramahkota1@gmail.com>
2020-05-02 19:59:32 +0700

    delete unused resource [club.js]

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,5 +1,3 @@
-import clubs from './clubs.js';
-
 class DataSource {
     static searchClub(keyword) {
         return fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`)
